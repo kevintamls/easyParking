8a9a131d6451160dd6530bb3fada45cb406d295f
= <kevintamls2@gmail.com>
2021-02-27 03:07:41 +0000

    Attempt to get map points to show

---
+++ convertCoordinates.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import json
+import geopandas as gpd
+import requests
+import geojson
+import geojsonio
+from shapely.geometry import Point
+import multiprocessing as mp
+import math
+import itertools
+
+parking_df = pd.read_csv('ON_STREET_PARK.csv')
+
+x_old_coor = list(parking_df['X_COOR'])
+y_old_coor = list(parking_df['Y_COOR'])
+print(len(x_old_coor))
+
+# Function to convert coordinates. Takes both x and y old coordinates
+def coordinateConvertion(x_old, y_old):
+    numberOfProcs = 100
+    out_q1 = mp.Queue()
+    def worker1(x_old, y_old, out_q1):
+        points = []
+        for i in range(len(x_old)):
+            url = 'http://www.geodetic.gov.hk/transform/v2/?inSys=hkgrid&outSys=wgsgeog&e=' + str(x_old[i]) + '&n=' + str(y_old[i])
+            r = requests.get(url)
+            data = r.json()
+            coor = Point((data['wgsLong'], data['wgsLat']))
+            #print(len(points))
+            points.append(coor)
+            #print(points)
+        out_q1.put(points)
+    
+    # Chunksize to split large list on multiple processes
+    chunksize = (int(math.ceil(len(x_old)/float(numberOfProcs))))
+    #print(chunksize)
+
+    # List to keep t rack of currently running processes
+    procs = []
+
+    # Multiprocesses execution for loop
+    for i in range(numberOfProcs):
+        p = mp.Process(
+            target = worker1,
+            args = (x_old[chunksize * i:chunksize * (i+1)], 
+                y_old[chunksize * i:chunksize * (i+1)],
+                out_q1)
+        )
+        procs.append(p)
+        p.start()
+    
+    results = []
+    for i in range(numberOfProcs):
+        results.append(out_q1.get())
+
+    for p in procs:
+        p.join()
+    
+    flatResults = list(itertools.chain.from_iterable(results))
+    print(flatResults)
+    d = {'geometry': flatResults}
+    #print(d)
+    gdf = gpd.GeoDataFrame(d, crs = 'EPSG:3857')
+    gdfJson = gdf.to_json()
+    with open('gdf.json', 'w') as f:
+        json.dump(gdfJson, f)
+
+    #geojsonio.display(gdf.to_json())
+
+coordinateConvertion(x_old_coor, y_old_coor)
--- static/script.js
+++ static/script.js
@@ -40,69 +40,72 @@ function indexLoad() {
 
   document.body.appendChild(wrapper1);
   //document.body.appendChild(mapCanvas);
-
-  mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW50YW1scyIsImEiOiJja2xtYjhrbTIwN2lhMnBvMzZzNnBsaGlrIn0.My-zIZ0u7uBFCamIm2qDzA';
-  var map = new mapboxgl.Map({
-  container: 'leftColumn',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [114.1, 22.3],
-  zoom: 9
-  });
 }
 
-/*$(document).ready(function () {
-  function fresh_ajax(gaugeRack, gaugePUE, tempGauge) {
+$(document).ready(function () {
+  function fresh_ajax() {
     $.ajax({
       type: "GET",
       url: "/_get_data/",
       success: function (resp) {
-        $('div#idcModelName1').text(resp.idcModelName1);
-        $('div#idcModelName2').text(resp.idcModelName2);
-        if (currentPage === 1 || currentPage === 2) {
-          $('div#inttemp').text(resp.int);
-          if (resp.idcModel === 2){
-            $('div#inttemp2').text(resp.int2);
-            $('div#rack1IntTemp').text(resp.rack1IntTemp);
-            $('div#rack2IntTemp').text(resp.rack2IntTemp);
-            $('div#rack1ExtTemp').text(resp.ext);
-            $('div#rack2ExtTemp').text(resp.ext2);
-          }
-          $('div#exttemp').text(resp.ext);
-          $('div#dispload').text(resp.load);
-          $('div#realCoolCap').text(resp.realCoolCap);
-          $('div#designLoadCap').text(resp.designLoadCap);
-          $('div#avgPUE').text(resp.avgPUE);
-          $('div#rack1Load').text(resp.rack1Load);
-          $('div#rack2Load').text(resp.rack2Load);
-          $('div#rack3Load').text(resp.rack3Load);
-          $('div#kWindicator').text('(kW)');
-          gaugeRack.value = resp.rawdata;
-          gaugePUE.value = resp.avgPUE;
-          tempGauge.value = resp.intGauge;
-        }
-        else if (currentPage === 3) {
-          $('div#serialLabel').text(resp.serialLabel);
-          $('div#rackConfigLabel').text(resp.rackConfigLabel);
-          $('div#designCapInfoLabel').text(resp.designCapInfoLabel);
-          $('div#controllerIPLabel').text(resp.controllerIPLabel);
-          $('div#serial').text(resp.serial);
-          $('div#rackConfig').text(resp.rackConfig);
-          $('div#designCapInfo').text(resp.designCapInfo);
-          $('div#controllerIP').text(resp.controllerIP);
-          }
+        var coordinateJson = JSON.parse(resp.coordinates)
+        console.log(typeof(resp.coordinates))
+        console.log(coordinateJson)
+        mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW50YW1scyIsImEiOiJja2xtYjhrbTIwN2lhMnBvMzZzNnBsaGlrIn0.My-zIZ0u7uBFCamIm2qDzA';
+        var map = new mapboxgl.Map({
+        container: 'leftColumn',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [114.1, 22.3],
+        zoom: 9
+        });
+        map.on('load', function() {
+          map.loadImage(
+            'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+            function (error, image) {
+            if (error) throw error;
+            map.addImage('custom-marker', image);
+            map.addSource('points', {
+              'type': 'geojson',
+              'data': {
+                "type": "Feature",
+                "geometry": {
+                "type": "Point",
+                "coordinates": [-77.0323, 38.9131]
+                },
+              "properties": {
+                "title": "test",
+                "market-symbol": "monument"
+              }
+            }})
+            map.addLayer({
+              'id': 'points',
+              'type': 'symbol',
+              'source': 'points',
+              'layout': {
+              'icon-image': 'custom-marker',
+              // get the title name from the source's "title" property
+              'text-field': ['get', 'title'],
+              'text-font': [
+              'Open Sans Semibold',
+              'Arial Unicode MS Bold'
+              ],
+              'text-offset': [0, 1.25],
+              'text-anchor': 'top'
+              }
+              });
+            }
+            
+          )
+
+        })
         }
       })
     }
 
-  fresh_ajax(window.gaugeDisplay, window.gaugeDisplay2, window.tempGauge2);
+  fresh_ajax();
 
-  var mutationObserver = new MutationObserver(function() {
-    fresh_ajax(window.gaugeDisplay, window.gaugeDisplay2, window.tempGauge2);
-  });
-
-  mutationObserver.observe(document.body, {childList: true});
-
-  window.setInterval(function () {
-    fresh_ajax(window.gaugeDisplay, window.gaugeDisplay2, window.tempGauge2);
-  }, 5000);
-});*/
+  
+  /*window.setInterval(function () {
+    fresh_ajax();
+  }, 5000);*/
+});
